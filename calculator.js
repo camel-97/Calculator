f90d59d5dd6e826a233051032a55196239b57750
camel-97 <cfmcmillan@icloud.com>
2025-05-26 19:04:50 +0100

    basic functions created and tested using array inputs

---
+++ calculator.js
@@ -0,0 +1,19 @@
+const add = function (array) {
+    return array.reduce((total, current) => total + current, 0);
+};
+
+const subtract  = function(array) {
+    return array.reduce((total, current) => total - current);
+};
+
+const multiply = function (array) {
+    return array.reduce((total, current) => total * current);
+};
+
+const divide = function(array)  {
+    return array.reduce((total, current) => total / current)
+}
+
+const operate = function(){
+    
+}
